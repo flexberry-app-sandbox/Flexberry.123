88522d5faf705217710d699640d35396a1484dc9
Flexberry-man <mail@flexberry.net>
2023-12-09 18:28:56 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -16,64 +16,61 @@ export default Controller.extend({
         }, {
           link: null,
           icon: 'list',
-          caption: i18n.t('forms.application.sitemap.123.caption'),
-          title: i18n.t('forms.application.sitemap.123.title'),
+          caption: i18n.t('forms.application.sitemap.заказ.caption'),
+          title: i18n.t('forms.application.sitemap.заказ.title'),
           children: [{
-            link: null,
-            caption: i18n.t('forms.application.sitemap.123.менеджеры.caption'),
-            title: i18n.t('forms.application.sitemap.123.менеджеры.title'),
-            children: [{
-              link: 'i-i-s-123-менеджер-l',
-              caption: i18n.t('forms.application.sitemap.123.менеджеры.i-i-s-123-менеджер-l.caption'),
-              title: i18n.t('forms.application.sitemap.123.менеджеры.i-i-s-123-менеджер-l.title'),
-              icon: 'archive',
-              children: null
-            }]
+            link: 'i-i-s-123-заказ-l',
+            caption: i18n.t('forms.application.sitemap.заказ.i-i-s-123-заказ-l.caption'),
+            title: i18n.t('forms.application.sitemap.заказ.i-i-s-123-заказ-l.title'),
+            icon: 'phone',
+            children: null
+          }]
+        }, {
+          link: null,
+          icon: 'list',
+          caption: i18n.t('forms.application.sitemap.справочник-по-товарам.caption'),
+          title: i18n.t('forms.application.sitemap.справочник-по-товарам.title'),
+          children: [{
+            link: 'i-i-s-123-поставщик-l',
+            caption: i18n.t('forms.application.sitemap.справочник-по-товарам.i-i-s-123-поставщик-l.caption'),
+            title: i18n.t('forms.application.sitemap.справочник-по-товарам.i-i-s-123-поставщик-l.title'),
+            icon: 'building',
+            children: null
           }, {
-            link: null,
-            caption: i18n.t('forms.application.sitemap.123.клиенты.caption'),
-            title: i18n.t('forms.application.sitemap.123.клиенты.title'),
-            children: [{
-              link: 'i-i-s-123-клиент-l',
-              caption: i18n.t('forms.application.sitemap.123.клиенты.i-i-s-123-клиент-l.caption'),
-              title: i18n.t('forms.application.sitemap.123.клиенты.i-i-s-123-клиент-l.title'),
-              icon: 'tasks',
-              children: null
-            }]
+            link: 'i-i-s-123-товар-l',
+            caption: i18n.t('forms.application.sitemap.справочник-по-товарам.i-i-s-123-товар-l.caption'),
+            title: i18n.t('forms.application.sitemap.справочник-по-товарам.i-i-s-123-товар-l.title'),
+            icon: 'address card',
+            children: null
           }, {
-            link: null,
-            caption: i18n.t('forms.application.sitemap.123.справочник-по-товарам.caption'),
-            title: i18n.t('forms.application.sitemap.123.справочник-по-товарам.title'),
-            children: [{
-              link: 'i-i-s-123-поставщик-l',
-              caption: i18n.t('forms.application.sitemap.123.справочник-по-товарам.i-i-s-123-поставщик-l.caption'),
-              title: i18n.t('forms.application.sitemap.123.справочник-по-товарам.i-i-s-123-поставщик-l.title'),
-              icon: 'address card',
-              children: null
-            }, {
-              link: 'i-i-s-123-товар-l',
-              caption: i18n.t('forms.application.sitemap.123.справочник-по-товарам.i-i-s-123-товар-l.caption'),
-              title: i18n.t('forms.application.sitemap.123.справочник-по-товарам.i-i-s-123-товар-l.title'),
-              icon: 'table',
-              children: null
-            }, {
-              link: 'i-i-s-123-производитель-l',
-              caption: i18n.t('forms.application.sitemap.123.справочник-по-товарам.i-i-s-123-производитель-l.caption'),
-              title: i18n.t('forms.application.sitemap.123.справочник-по-товарам.i-i-s-123-производитель-l.title'),
-              icon: 'briefcase',
-              children: null
-            }]
-          }, {
-            link: null,
-            caption: i18n.t('forms.application.sitemap.123.заказ.caption'),
-            title: i18n.t('forms.application.sitemap.123.заказ.title'),
-            children: [{
-              link: 'i-i-s-123-заказ-l',
-              caption: i18n.t('forms.application.sitemap.123.заказ.i-i-s-123-заказ-l.caption'),
-              title: i18n.t('forms.application.sitemap.123.заказ.i-i-s-123-заказ-l.title'),
-              icon: 'chart line',
-              children: null
-            }]
+            link: 'i-i-s-123-производитель-l',
+            caption: i18n.t('forms.application.sitemap.справочник-по-товарам.i-i-s-123-производитель-l.caption'),
+            title: i18n.t('forms.application.sitemap.справочник-по-товарам.i-i-s-123-производитель-l.title'),
+            icon: 'calendar',
+            children: null
+          }]
+        }, {
+          link: null,
+          icon: 'list',
+          caption: i18n.t('forms.application.sitemap.клиенты.caption'),
+          title: i18n.t('forms.application.sitemap.клиенты.title'),
+          children: [{
+            link: 'i-i-s-123-клиент-l',
+            caption: i18n.t('forms.application.sitemap.клиенты.i-i-s-123-клиент-l.caption'),
+            title: i18n.t('forms.application.sitemap.клиенты.i-i-s-123-клиент-l.title'),
+            icon: 'tags',
+            children: null
+          }]
+        }, {
+          link: null,
+          icon: 'list',
+          caption: i18n.t('forms.application.sitemap.менеджеры.caption'),
+          title: i18n.t('forms.application.sitemap.менеджеры.title'),
+          children: [{
+            link: 'i-i-s-123-менеджер-l',
+            caption: i18n.t('forms.application.sitemap.менеджеры.i-i-s-123-менеджер-l.caption'),
+            title: i18n.t('forms.application.sitemap.менеджеры.i-i-s-123-менеджер-l.title'),
+            children: null
           }]
         }
       ]
